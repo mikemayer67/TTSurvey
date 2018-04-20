0c335e060b2f8450bd3085a654f479ebf4a2be0d
mike <mikemayer67@vmwishes.com>
2018-04-20 09:59:13 -0400

    Fixed survey php filename

--- tt_page.php
+++ tt_page.php
@@ -4,5 +4,5 @@
 <noscript>
   <div class=tt-noscript>This site works best with JavaScript enabled</div>
   <p class=tt-noscript>If you cannot (or prefer not) to turn on Javascript, you may need to complete a paper copy of the survey.<br>
-  You can download a PDF version <a href='files/tt_survey_2017.pdf'>here</a>.</p>
+  You can download a PDF version <a href='files/CTS_2017_TT_Survey.pdf'>here</a>.</p>
 </noscript>
